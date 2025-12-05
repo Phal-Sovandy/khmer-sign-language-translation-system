7c7b732c941b274e85093ba96c1aa6587b60af8a
monotai <wannwattqarl@gmail.com>
2025-12-06 03:57:32 +0700

    update

--- backend/server_k.py
+++ backend/server_k.py
@@ -20,7 +20,9 @@ CORS(app, resources={r"/*": {
         "http://127.0.0.1:5000",
         "http://localhost:5000",
         "http://127.0.0.1:5173",
-        "http://localhost:5173"
+        "http://localhost:5173",
+        "http://127.0.0.1:3000",
+        "http://localhost:3000",
     ]
 }})
 
@@ -172,4 +174,4 @@ def predict_image():
 # -------------------------
 if __name__ == "__main__":
     print("✅ Starting Flask server...")
-    app.run(host='0.0.0.0', port=5000, debug=True)
+    app.run(host='0.0.0.0', port=3000, debug=True)
